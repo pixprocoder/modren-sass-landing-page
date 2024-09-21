0a91fe8aab77fd1cce04ed133c0344551f7f0d1d
pixprocoder <makkobir79@gmail.com>
2024-09-21 23:28:31 +0300

    testimonials section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
     </main>
   );
 };
